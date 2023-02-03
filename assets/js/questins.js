6b29198015f7d618ceb71971337ab413191d64ca
Maeve <mariya.kuryakulova@gmail.com>
2023-02-03 21:13:25 +0000

    add-event-listener
    Declared main variables. Added EventListener to the starter button so the timer starts and the first question appears

---
+++ assets/js/questins.js
@@ -0,0 +1,35 @@
+let startQuize = document.getElementById("start");
+let timer = document.getElementById("time");
+let startScreen = document.getElementById("start-screen");
+let questionsScreen = document.getElementById("questions");
+let resultsScreen = document.getElementById("end-screen");
+let questions = ['Inside which HTML element do we put the JavaScript?', 'Where is the correct place to insert a JavaScript?', 'What is the correct syntax for referring to an external script called "xxx.js"?']
+let answers = [['<script>', '<scripting>', '<js>', '<javascript>'], ['The <head> section', 'The <body> section', 'Both the <head> section and the <body> section are correct', 'None of the above'], ['<script src="xxx.js">', '<script href="xxx.js">', '<script name="xxx.js">', 'None of the above']]
+
+startQuize.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let countdown = 30;
+
+    startScreen.classList.add("hide");
+    questionsScreen.classList.remove("hide");
+    
+    let interval = setInterval(function () {
+        timer.textContent = countdown;
+        countdown--;
+
+        if (countdown === 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
