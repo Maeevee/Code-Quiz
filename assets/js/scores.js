c382aeac9f40c9905441ba0e33c1e85c583ae907
Maeve <mariya.kuryakulova@gmail.com>
2023-02-05 23:36:20 +0000

    add-li-tag
    Declared new variables and created <li> elements that contain names and points

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,12 @@
+let initials = document.getElementById("initials");
+let submitButton = document.getElementById("submit");
+let highscores = document.getElementById("highscores");
+// let names = [];
+// let points = [];
 
+var names = localStorage.getItem("name");
+var points = localStorage.getItem("Current result");
+
+var li = document.createElement("li");
+li.innerHTML = names + " - " + points;
+highscores.appendChild(li);
