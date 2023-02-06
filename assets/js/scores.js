8a1a4d025ccd30f6955e9857e205574439706130
Maeve <mariya.kuryakulova@gmail.com>
2023-02-06 00:29:15 +0000

    add-clear-highscores-EventListener
    Added EventListener to 'clear highscores' button so click clear local storage and <ol> list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -10,3 +10,9 @@ var points = localStorage.getItem("Current result");
 var li = document.createElement("li");
 li.innerHTML = names + " - " + points;
 highscores.appendChild(li);
+
+document.getElementById("clear").addEventListener("click", function() {
+    localStorage.clear();
+    highscores.innerHTML = "";
+
+});
