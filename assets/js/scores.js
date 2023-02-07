673df3374dfdd662226a9925d3e631699b7a08f1
Maeve <mariya.kuryakulova@gmail.com>
2023-02-07 00:23:06 +0000

    debug-highscores
    Hided highscores list until there is no data

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,16 +1,13 @@
 let initials = document.getElementById("initials");
 let submitButton = document.getElementById("submit");
 let highscores = document.getElementById("highscores");
-// let names = [];
-// let points = [];
 
-var names = localStorage.getItem("name");
-var points = localStorage.getItem("Current result");
+// Hide highscores if there are no values in local storage
+if (names === null) {
+    highscores.classList.add("hide");
+}
 
-var li = document.createElement("li");
-li.innerHTML = names + " - " + points;
-highscores.appendChild(li);
-
+// Clear local storage
 document.getElementById("clear").addEventListener("click", function() {
     localStorage.clear();
     highscores.innerHTML = "";
