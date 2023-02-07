a53455b68faf918eb28307867d4a67f8da2a711d
Maeve <mariya.kuryakulova@gmail.com>
2023-02-07 00:26:55 +0000

    get-items-from-localStorage
    Retrieved values from local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,6 +2,10 @@ let initials = document.getElementById("initials");
 let submitButton = document.getElementById("submit");
 let highscores = document.getElementById("highscores");
 
+// Retrieve values from local storage
+let names = localStorage.getItem("name");
+let points = localStorage.getItem("Current result");
+
 // Hide highscores if there are no values in local storage
 if (names === null) {
     highscores.classList.add("hide");
