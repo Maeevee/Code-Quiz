050cda625b44820da62c5615ecf9e44b2c69e379
Maeve <mariya.kuryakulova@gmail.com>
2023-02-07 18:00:31 +0000

    debug-highscores
    Added filter method to delete 'null' values from high scores as the previous solution did not work correctly

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -14,26 +14,30 @@ let storedResultScoresArray = JSON.parse(localStorage.getItem("resultScoresArray
 storedNamesArray.push(names);
 storedResultScoresArray.push(points);
 
+// Filter out null values
+const namesWithoutNulls = storedNamesArray.filter(item => item !== null);
+const resultScoresWithoutNulls = storedResultScoresArray.filter(item => item !== null);
+
 // Store arrays back in local storage
-localStorage.setItem("namesArray", JSON.stringify(storedNamesArray));
-localStorage.setItem("resultScoresArray", JSON.stringify(storedResultScoresArray));
+localStorage.setItem("namesArray", JSON.stringify(namesWithoutNulls));
+localStorage.setItem("resultScoresArray", JSON.stringify(resultScoresWithoutNulls));
 
 // Sort result scores in descending order
-let indices = [...Array(storedResultScoresArray.length).keys()].sort((a, b) => {
-    return storedResultScoresArray[b] - storedResultScoresArray[a];
+let indices = [...Array(resultScoresWithoutNulls.length).keys()].sort((a, b) => {
+    return resultScoresWithoutNulls[b] - resultScoresWithoutNulls[a];
     });
 
 // Display values in a list
-for (let i = 0; i < storedNamesArray.length; i++) {
+for (let i = 0; i < namesWithoutNulls.length; i++) {
     let li = document.createElement("li");
-    li.innerHTML = storedNamesArray[indices[i]] + ": " + storedResultScoresArray[indices[i]];
+    li.innerHTML = namesWithoutNulls[indices[i]] + ": " + resultScoresWithoutNulls[indices[i]];
     highscores.appendChild(li);
     }
 
 // Hide highscores if there are no values in local storage
-if (names === null) {
-    highscores.classList.add("hide");
-}
+// if (names === null) {
+//     highscores.classList.add("hide");
+// }
 
 // Clear local storage
 document.getElementById("clear").addEventListener("click", function() {
